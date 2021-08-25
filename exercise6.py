0c588ccf359d90eeaf5f9991d5754b8fea9ec51d
vkarhula <vkarhula@gmail.com>
2021-08-25 13:27:43 +0300

    Update exercise6.py
    Task description added.

--- exercise6.py
+++ exercise6.py
@@ -5,6 +5,19 @@ Created on Mon Mar 30 15:47:00 2020
 @author: OMISTAJA
 """
 
+# Exercise 6 (triple square)
+# Write two functions: triple and square. Function triple multiplies its parameter by three. 
+# Function square raises its parameter to the power of two. For example, we have equalities triple(5)==15 and square(5)==25.
+
+# Part 1.
+# In the main function write a for loop that iterates through values 1 to 10, and for each value prints its triple and its square. 
+# The output should be as follows:
+# triple(1)==3 square(1)==1
+# triple(2)==6 square(2)==4
+
+# Part 2.
+# Now modify this for loop so that it stops iteration when the square of a value is larger than the triple of the value, 
+# without printing anything in the last iteration.
 
 #help(print)
 
