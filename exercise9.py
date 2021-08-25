0bc5158526a94f739f2da73a6bc8257b16a9c9b1
vkarhula <vkarhula@gmail.com>
2021-08-25 13:41:27 +0300

    Update exercise9.py
    Task description added.

--- exercise9.py
+++ exercise9.py
@@ -2,6 +2,18 @@
 """
 Created on Wed Apr  1 10:31:44 2020
 
+Exercise 9 (merge)
+Suppose we have two lists L1 and L2 that contain integers which are sorted in ascending order. 
+Create a function merge that gets these lists as parameters and returns a new sorted list L 
+that has all the elements of L1 and L2. So, len(L) should equal to len(L1)+len(L2). 
+Do this using the fact that both lists are already sorted. You can’t use the sorted function 
+or the sort method in implementing the merge method. You can however use these sorted 
+in the main function for creating inputs to the merge function. Test with a couple of examples 
+in the main function that your solution works correctly.
+
+Note: In Python argument lists are passed by reference to the function, they are not copied! 
+Make sure you don’t modify the original lists of the caller.
+
 @author: OMISTAJA
 """
 
