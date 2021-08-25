cbb827cc6bf5cffaf460362975958038884fdb12
vkarhula <vkarhula@gmail.com>
2021-08-25 13:15:36 +0300

    Update exercise10.py
    Description of the exercise 10 added.

--- exercise10.py
+++ exercise10.py
@@ -5,6 +5,16 @@ Created on Tue Apr  7 14:01:02 2020
 @author: OMISTAJA
 """
 
+# Exercise 10 (detect ranges)
+# Create a function named detect_ranges that gets a list of integers as a parameter. The function should then sort this list, 
+# and transform the list into another list where pairs are used for all the detected intervals. So 3,4,5,6 is replaced by the pair (3,7). 
+# Numbers that are not part of any interval result just single numbers. The resulting list consists of these numbers and pairs, 
+#separated by commas. An example of how this function works:
+
+# print(detect_ranges([2,5,4,8,12,6,7,10,13]))
+# [2,(4,9),10,(12,14)]
+#Note that the second element of the pair does not belong to the range. This is consistent with the way Python’s range function works. 
+# You may assume that no element in the input list appears multiple times.
 
 def detect_ranges(*lst):
     #print(lst)
