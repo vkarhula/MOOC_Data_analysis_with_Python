fac781e3ee4c5aa3475254523399b89610dd5072
vkarhula <vkarhula@gmail.com>
2021-08-25 21:21:12 +0300

    Update exercise8.py
    Minor changes.

--- exercise8.py
+++ exercise8.py
@@ -2,6 +2,17 @@
 """
 Created on Tue Mar 31 15:41:30 2020
 
+Exercise 8 (solve quadratic)
+In mathematics, the quadratic equation ax2+bx+c=0 can be solved with the formula x=−b±b2−4ac√2a.
+
+Write a function solve_quadratic, that returns both solutions of a generic quadratic as a pair (2-tuple) 
+when the coefficients are given as parameters. It should work like this:
+
+print(solve_quadratic(1,-3,2))
+(2.0,1.0)
+print(solve_quadratic(1,2,1))
+(-1.0,-1.0)
+
 @author: OMISTAJA
 """
 
@@ -13,16 +24,6 @@ import math
 #lst = [1,-3,2]
 #solve_quadratic(*lst)
 
-# Exercise 8 (solve quadratic)
-# In mathematics, the quadratic equation ax2+bx+c=0 can be solved with the formula x=−b±b2−4ac√2a.
-
-# Write a function solve_quadratic, that returns both solutions of a generic quadratic as a pair (2-tuple) 
-# when the coefficients are given as parameters. It should work like this:
-
-# print(solve_quadratic(1,-3,2))
-# (2.0,1.0)
-# print(solve_quadratic(1,2,1))
-# (-1.0,-1.0)
 
 def solve_quadratic(*lst):
     #print(f"{lst}")
