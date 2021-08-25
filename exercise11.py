9aab50d5a60f3afc425cf05ce1b2fb3a371fae53
vkarhula <vkarhula@gmail.com>
2021-08-25 13:46:15 +0300

    Update exercise11.py
    Task description added.

--- exercise11.py
+++ exercise11.py
@@ -2,6 +2,14 @@
 """
 Created on Tue Apr  7 15:52:00 2020
 
+Exercise 11 (interleave)
+Write function interleave that gets arbitrary number of lists as parameters. You may assume that 
+all the lists have equal length. The function should return one list containing all the elements 
+from the input lists interleaved. Test your function from the main function of the program.
+
+Example: interleave([1,2,3], [20,30,40], ['a', 'b', 'c']) should return [1, 20, 'a', 2, 30, 'b', 3, 40, 'c']. 
+Use the zip function to implement interleave. Remember the extend method of list objects.
+
 @author: OMISTAJA
 """
 
