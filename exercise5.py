48c7d882be989b17b7ebd027d84ed6694afe1288
vkarhula <vkarhula@gmail.com>
2021-08-25 21:16:58 +0300

    Update exercise5.py
    Minor changes.

--- exercise5.py
+++ exercise5.py
@@ -2,14 +2,16 @@
 """
 Created on Mon Mar 30 15:17:14 2020
 
+Exercise 5 (two dice)
+Let us consider throwing two dice. (A dice can give a value between 1 and 6.) Use two nested for loops 
+in the main function to iterate through all possible combinations the pair of dice can give. 
+There are 36 possible combinations. Print all those combinations as (ordered) pairs that sum to 5. 
+For example, your printout should include the pair (2,3). Print one pair per line.
+
 @author: OMISTAJA
 """
 
-# Exercise 5 (two dice)
-# Let us consider throwing two dice. (A dice can give a value between 1 and 6.) Use two nested for loops 
-# in the main function to iterate through all possible combinations the pair of dice can give. 
-# There are 36 possible combinations. Print all those combinations as (ordered) pairs that sum to 5. 
-# For example, your printout should include the pair (2,3). Print one pair per line.
+
 
 for i in range(1,7):
     for j in range(1,7):
